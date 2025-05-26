31a42e2b256f4a7498c96f0656409efe7c4f6c84
Stephen Funk <stephenlfunk@gmail.com>
2025-05-26 06:06:36 -0400

    Layering the frontend widget

--- src/mosf/static/widget.js
+++ src/mosf/static/widget.js
@@ -1,3 +1,19 @@
 function render({ model, el }) {
+  let connected = model.get("connected");
+  const label = model.get("label");
+  const alias = model.get("alias");
+  let div = document.createElement("div");
+
+  div.innerHTML = `
+    <button class="login">${label}</button>
+    ${!connected ? "" : 
+        `<span class="status">Connected to "${alias}"</span>`
+      }
+  `;
+
+  div.addEventListener('click', () => {
+    
+  })
   
+  el.appendChild(div);
 }
--- src/mosf/widget/widget.py
+++ src/mosf/widget/widget.py
@@ -1,15 +1,16 @@
 from dataclasses import dataclass
-from typing import Optional, NewType, Literal
+from urllib.parse import urlparse
+from typing import Optional, NewType, Literal, Any
 
 import anywidget
 import traitlets
 from requests_oauthlib import OAuth2Session
 
 
-# SerializedState = NewType("SerializedState", str)
+SerializedState = NewType("SerializedState", str)
 # """A JSON serialized, URL-encoded State"""
 
-# TimestampMilli = NewType("TimestampMilli", int)
+TimestampMilli = NewType("TimestampMilli", int)
 
 # @dataclass(slots=True)
 # class State:
@@ -17,27 +18,53 @@ from requests_oauthlib import OAuth2Session
 #     ...
 
 
-# @dataclass
-# class AccessToken:
-#     access_token: str
-#     signature: Optional[str]
-#     scope: list[str]
-#     id_token: str
-#     instance_url: str
-#     id: str
-#     issued_at: TimestampMilli
-#     refresh_token: Optional[str]
-#     sfdc_site_url: Optional[str]
-#     sfdc_site_id: Optional[str]
-#     state: Optional[SerializedState]
+@dataclass
+class AccessToken:
+    access_token: str
+    signature: Optional[str]
+    scope: list[str]
+    id_token: str
+    instance_url: str
+    id: str
+    issued_at: TimestampMilli
+    refresh_token: Optional[str]
+    sfdc_site_url: Optional[str]
+    sfdc_site_id: Optional[str]
+    state: Optional[SerializedState]
 
-#     token_type: Literal["Bearer"] = "Bearer"
+    token_type: Literal["Bearer"] = "Bearer"
 
 class SFLoginWidget(anywidget.AnyWidget): 
     """Salesforce Login Anywidget"""
     
     _esm = "static/widget.js"
     _css = "static/widget.css"
+
+    def __init__(self, label: str = "Sign in to Salesforce", alias: Optional[str] = None):
+        self.label = label
+        self._alias = alias
     
     connected = traitlets.Bool(False).tag(sync=True)
     session: OAuth2Session | None = None
+
+    @property
+    def access_token(self) -> AccessToken | None:
+        if self.session is None:
+            return None
+        access_token_dict: dict[str, Any] = self.session.access_token # type: ignore[access_token]
+        return AccessToken(**access_token_dict)
+
+    @property
+    def instance_url(self) -> str | None:
+        if self.access_token is not None:
+            return self.access_token.instance_url
+    
+    @property
+    def alias(self) -> str | None:
+        if (
+            self.access_token is None 
+            or (hostname := urlparse(self.access_token.instance_url).hostname) is None
+        ):
+            return None
+        [instance, *_] = hostname.split('.')
+        return self._alias or instance
