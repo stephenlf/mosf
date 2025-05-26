620c7c078b2659d0ae90bf42475343e2e18cc2c3
Stephen Funk <stephenlfunk@gmail.com>
2025-05-26 05:30:31 -0400

    Adding some structure to the SFLoginWidget

---
+++ src/mosf/widget/widget.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass
+from typing import Optional, NewType, Literal
+
+import anywidget
+import traitlets
+from requests_oauthlib import OAuth2Session
+
+
+SerializedState = NewType("SerializedState", str)
+"""A JSON serialized, URL-encoded State"""
+
+TimestampMilli = NewType("TimestampMilli", int)
+
+@dataclass(slots=True)
+class State:
+    """"""
+    ...
+
+
+# @dataclass
+# class AccessToken:
+#     access_token: str
+#     signature: Optional[str]
+#     scope: list[str]
+#     id_token: str
+#     instance_url: str
+#     id: str
+#     issued_at: TimestampMilli
+#     refresh_token: Optional[str]
+#     sfdc_site_url: Optional[str]
+#     sfdc_site_id: Optional[str]
+#     state: Optional[SerializedState]
+
+#     token_type: Literal["Bearer"] = "Bearer"
+
+class SFLoginWidget(anywidget.AnyWidget):
+    _esm = "static/widget.js"
+    _css = "static/widget.css"
+    
+    connected = traitlets.Bool(False).tag(sync=True)
+    session: OAuth2Session | None = None
